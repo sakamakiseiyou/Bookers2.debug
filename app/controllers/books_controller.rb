932aecd0bbffe9fa398d41e284580e3f4769bbdf
EC2 Default User <ec2-user@ip-172-31-8-12.ap-northeast-1.compute.internal>
2023-12-05 08:29:00 +0000

    end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
+   before_action :ensure_correct_user, only: [:update, :edit ]
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+
   end
 
   def create
@@ -32,15 +36,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body, :image)
+  end
+
+  def ensure_correct_user
+    book = Book.find(params[:id])
+    books = book.user
+    unless books == current_user
+      redirect_to books_path
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "7bfb4ee21beb43d6811a2872ff8f8f2f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
